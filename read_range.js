64eb3fcd4282f4b767bef375642fa377815706d0
marcoalfoonso <marcoalfoonso.rodcal@gmail.com>
2026-03-01 18:27:55 -0600

    se conectó a mqtt

--- read_range.js
+++ read_range.js
@@ -1,9 +1,28 @@
+const client = mqtt.connect("wss://e4f0d50b37b04ea79745872566f605ff.s1.eu.hivemq.cloud:8884/mqtt",{
+    clientId: "web_" + Math.random().toString(16).slice(2, 10),
+    username: "MarcoA",
+    password: "HATeR3__",
+    clean: true
+});
+
+client.on("connect", () => {
+  console.log("Connecting with Outh");
+});
+
+client.on("error", (err) => {
+  console.error("Error:", err);
+});
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const range = document.getElementById("values");
 
     range.oninput = () => {
         console.log(range.value)
+
+        if(client.connected){
+            client.publish("q1",range.value)
+        }
     }
 
 });
